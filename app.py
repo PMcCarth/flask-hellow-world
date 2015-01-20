07eb9ae0c724a727becd2f13c585afcc24707472
Patrick McCarthy <pattehuber@gmail.com>
2015-01-19 22:00:27 -0800

    Updated with debugging

--- app.py
+++ app.py
@@ -5,9 +5,21 @@ from flask import Flask
 # Create application object #
 app = Flask(__name__)
 
+# error handling #
+app.config["DEBUG"] = True
+
 # Use decorators to link the function to a url #
 @app.route("/")
 @app.route("/hello")
+@app.route("/test/<search_query>")
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
+def search(search_query):
+    return search_query
 
 # define the view using a function, which returns a string #
 def hello_world():
